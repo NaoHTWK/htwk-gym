d36c5b3f5b2fe73d1cee2b596abd4bc0030e451a
Felix Loos <felix.loos@t-online.de>
2025-11-11 19:48:49 +0100

    added dribble training

--- envs/__init__.py
+++ envs/__init__.py
@@ -3,3 +3,4 @@ from envs.T1.kicking import Kicking
 from envs.K1.parameter_walk import ParameterWalk
 from envs.T1.parameter_walk import ParameterWalk
 from envs.T1.high_level_traning.kicking_high_level import KickingHighLevel
+from envs.T1.high_level_traning.dribble_high_level import DribbleHighLevel
